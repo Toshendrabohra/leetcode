3193ae97bcbc5b397bc18b6e5ffeca95bd2459e7
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-01-08 17:41:47 +0530

    Time: 271 ms (9.48%), Space: 14.4 MB (23.39%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -6,32 +6,28 @@ public:
             return true;
         return false;
     }
-    int dfs(int r1x,int r1y,int r2x,int r2y,int &n,int &m,vector<vector<vector<vector<int>>>> &dp,vector<int> &dy,vector<vector<int>>& grid)
+    int dfs(int rx,int r1y,int r2y,int &n,int &m,vector<vector<vector<int>>> &dp,vector<int> &dy,vector<vector<int>>& grid)
     {
-       // cout<<r1x<<" "<<r1y<<" "<<r2x<<" "<<r2y<<endl;
-        if(dp[r1x][r1y][r2x][r2y]!=-1)
-            return dp[r1x][r1y][r2x][r2y];
+        if(dp[rx][r1y][r2y]!=-1)
+            return dp[rx][r1y][r2y];
         int mx=0;
         for(int i=0;i<3;i++)
         {
             for(int j=0;j<3;j++)
             {
-                if(valid(r1x+1,r1y+dy[i],n,m) && valid(r2x+1,r2y+dy[j],n,m))
+                if(valid(rx+1,r1y+dy[i],n,m) && valid(rx+1,r2y+dy[j],n,m))
                 {
-                    mx=max(mx,grid[r1x+1][r1y+dy[i]]+((r1x==r2x && r1y+dy[i]==r2y+dy[j])?0:grid[r2x+1][r2y+dy[j]]+dfs(r1x+1,r1y+dy[i],r2x+1,r2y+dy[j],n,m,dp,dy,grid)));
+                    mx=max(mx,grid[rx+1][r1y+dy[i]]+(( r1y+dy[i]==r2y+dy[j])?0:grid[rx+1]                      [r2y+dy[j]]+dfs(rx+1,r1y+dy[i],r2y+dy[j],n,m,dp,dy,grid)));
                 }
             }
         }
-         return dp[r1x][r1y][r2x][r2y]=mx;
-        
+         return dp[rx][r1y][r2y]=mx;   
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<vector<int>>>> dp(n,vector<vector<vector<int>>>
-                                              (m,vector<vector<int>>
-                                              (n,vector<int>(m,-1))));
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
         vector<int > dy={-1,0,1};
-       return grid[0][0]+grid[0][m-1]+dfs(0,0,0,m-1,n,m,dp,dy,grid);
+       return grid[0][0]+grid[0][m-1]+dfs(0,0,m-1,n,m,dp,dy,grid);
     }
 };
