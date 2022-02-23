19458369771fc3a747c8a0fb7970a2a0844d99d9
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-02-24 00:32:51 +0530

    Time: 9 ms (46.46%), Space: 8.9 MB (23.69%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* dfs(Node* node,vector<int> &dp,map<int,Node *> &m)
+    { 
+        if(dp[node->val])
+            return m[node->val];
+        dp[node->val]=1;
+        Node *cur=new Node(node->val);
+        m[node->val]=cur;
+        for(auto &i:node->neighbors)
+        {
+            cur->neighbors.push_back(dfs(i,dp,m));
+        }
+        return cur;
+    }
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)
+            return node;
+        vector<int> dp(1001,0);
+        map<int,Node*> m;
+        return dfs(node,dp,m);
+    }
+};
