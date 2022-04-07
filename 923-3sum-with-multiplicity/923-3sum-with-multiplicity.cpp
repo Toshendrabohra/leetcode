bfa79e4022de4428b987664ddca3f4bb6714dca2
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-04-07 12:50:16 +0530

    Time: 76 ms (68.73%), Space: 16.9 MB (5.84%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        
+        int n=arr.size();
+        int mod=1e9+7;
+
+        vector<long long int> freq(101,0);
+        for(int i=0;i<n;i++)   
+        {
+            freq[arr[i]]++;
+        }
+        map<int,int> hash;
+        int ans=0;
+        for(int i=0;i<=100;i++)
+        {
+            int l=0,r=100;
+            while(l<=r)
+            {
+                if(l+r==target-i)
+                {
+                    
+                 long long   int freq1=freq[l];
+                 long long   int freq2=freq[r];
+                    int cnt=1;
+                    if(l==i)
+                        freq1--;
+                    if(r==i)
+                        freq2--;
+                    if(l==r)
+                        freq2--;
+                    vector<int> x={l,r,i};
+                    sort(x.begin(),x.end());
+                    int hash_val=x[0]*11000+x[1]*101+x[2];
+                    long long int factor=1;
+                    
+                    if(hash[hash_val])
+                        goto here;
+                    hash[hash_val]=1;
+                    if(x[0]==x[1])
+                        cnt++;
+                    if(x[2]==x[1])
+                        cnt++;
+                    
+                    while(cnt!=1)
+                    {
+                        factor=factor*cnt;
+                        cnt--;
+                    }
+                    
+                    ans=(ans+((freq[i]*freq1*freq2)/factor)%mod)%mod;
+                    here:
+                   // cout<<i<<" "<<l<<" "<<r<<" "<<(freq[i]*freq1*freq2)/factor<<endl;
+                    l++;
+                    r--;
+                }
+                else if(l+r<target-i)
+                    l++;
+                else
+                    r--;
+            }
+            
+        }
+        return ans;
+    }
+};
