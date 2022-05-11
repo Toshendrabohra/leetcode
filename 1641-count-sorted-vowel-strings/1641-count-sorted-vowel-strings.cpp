d47a7e9faf3579206f949ca1a9abbcf819c49dd2
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-05-11 17:18:56 +0530

    Time: 5 ms (24.57%), Space: 6.5 MB (12.14%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    int count(int curPos , int &n , int vowel, vector<vector<int>> &dp)
-    {
-        if(curPos == n)
-            return 1;
+//     int count(int curPos , int &n , int vowel, vector<vector<int>> &dp)
+//     {
+//         if(curPos == n)
+//             return 1;
         
-        if(dp[curPos][vowel] != -1)
-            return dp[curPos][vowel];
+//         if(dp[curPos][vowel] != -1)
+//             return dp[curPos][vowel];
         
-        dp[curPos][vowel]=0;
+//         dp[curPos][vowel]=0;
         
-        for( int i = vowel ; i < 5 ; i++)
-        {
-            dp[curPos][vowel]+=count ( curPos+1 , n , i , dp);
-        }
+//         for( int i = vowel ; i < 5 ; i++)
+//         {
+//             dp[curPos][vowel]+=count ( curPos+1 , n , i , dp);
+//         }
         
-        return dp[curPos][vowel];
-    }
+//         return dp[curPos][vowel];
+//     }
     int countVowelStrings(int n) {
-        vector<vector<int>> dp (n+1, vector<int> (5,-1));
-        map<char,int> hash;
-        vector<char> vowels = {'a' , 'e' , 'i' , 'o' , 'u'};
+        vector<vector<int>> dp (n+1, vector<int> (5,0));
         
-        for(int i = 0 ; i < 5 ; i++)
-            hash[vowels[i]] = i;
+        dp[0][0]=1;
+        int ans=0;
         
-        return count(0 , n , 0 , dp);
-        
+        for( int i = 1; i <= n ; i++ )
+        {
+            for ( int j = 0 ; j < 5 ; j++)
+            {
+                dp[i][j]+=dp[i-1][j];
+                
+                if(j>0)
+                    dp[i][j]+=dp[i][j-1];
+                
+                if(i==n)
+                ans+=dp[n][j];
+            }
+        }
+    
+        return ans;
     }
 };
