7d5eb7d4f184a35e08dfefa54b9d812aed561053
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-05-16 23:58:41 +0530

    Time: 100 ms (51.38%), Space: 19.3 MB (83.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -40,7 +40,33 @@ public:
         return -1;
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-      int n=grid.size()  ;
-       return bfs(n,grid);
+       int n = grid.size();
+
+        queue<pair<int,int>> q;
+        if(grid[0][0] || grid[n - 1][n - 1]) return -1;
+
+        q.push({0,0});
+        grid[0][0] = 1;
+
+        int dx[] = {-1,1,0,0,-1,-1,1,1};
+        int dy[] = {0,0,1,-1,-1,1,1,-1};
+
+        while(q.size()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            for(int i = 0;i<8;i++){
+                int x1 = x + dx[i];
+                int y1 = y + dy[i];
+
+                if(x1 >= 0 and y1 >= 0 and x1 < n and y1 < n and !grid[x1][y1]){
+                    grid[x1][y1] = grid[x][y] + 1;
+                    q.push({x1,y1});
+                }
+            }
+        }
+
+        return grid[n - 1][n - 1] ? grid[n - 1][n - 1] : -1;
     }
 };
