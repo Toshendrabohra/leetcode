d08d2110b219a8a5d9daf52c5953bdebb2a829fa
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-05-28 03:35:13 +0530

    Time: 79 ms (75.06%), Space: 61.6 MB (27.10%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,19 +1,30 @@
-class Solution {
-public:
-    bool increasingTriplet(vector<int>& nums) {
-        vector<int> dp;
-        dp.push_back(nums[0]);
-
-        for (int i = 1; i < nums.size(); i++)
+class Solution
+{
+    public:
+        bool increasingTriplet(vector<int> &nums)
         {
-            int x = lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
 
-            if (x != dp.size())
-                dp[x] = nums[i];
-            else
-                dp.push_back(nums[i]);
+            vector<int> x;
+            x = {nums[0], INT_MIN};
+
+            for (int i = 1; i < nums.size(); i++) {
+                if (x[0] >= x[1])
+                {
+                    if (x[0] < nums[i])
+                        x[1] = nums[i];
+                    else
+                        x[0] = min(x[0], nums[i]);
+                }
+                else if (nums[i] > x[1])
+                    return true;
+                else if( nums[i]<= x[0])
+                {
+                    x[0]=nums[i];
+                }
+                else
+                    x[1]=nums[i];
+            }
+            
+            return false;
         }
-
-        return dp.size()>=3;
-    }
 };
