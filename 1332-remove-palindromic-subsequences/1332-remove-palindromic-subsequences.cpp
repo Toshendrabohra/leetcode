3cca672d53f939d38ce9534d7c59732e26045fa5
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-08 22:22:31 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -5,13 +5,13 @@ class Solution
         {
             int cnt = 0;
 
-            for (int i = 0; i < s.size(); i++)
+            for (int i = 0; i < s.size() / 2; i++)
             {
                 if (s[i] == s[s.size() - 1 - i])
                     cnt++;
             }
 
-            if (cnt == s.size())
+            if (cnt == s.size()/2)
                 return 1;
             return 2;
         }
