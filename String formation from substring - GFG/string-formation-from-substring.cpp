8e6f71578486116d5575603b5f943a374bd3d108
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-13 22:59:06 +0530

    Added solution - LeetHub

---
+++ String formation from substring - GFG/string-formation-from-substring.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void getzarr(string str, vector<int> &Z)
+{
+    int n = str.length();
+    int L, R, k;
+ 
+    // [L,R] make a window which matches with prefix of s
+    L = R = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        // if i>R nothing matches so we will calculate.
+        // Z[i] using naive way.
+        if (i > R)
+        {
+            L = R = i;
+ 
+            // R-L = 0 in starting, so it will start
+            // checking from 0'th index. For example,
+            // for "ababab" and i = 1, the value of R
+            // remains 0 and Z[i] becomes 0. For string
+            // "aaaaaa" and i = 1, Z[i] and R become 5
+            while (R<n && str[R-L] == str[R])
+                R++;
+            Z[i] = R-L;
+            R--;
+        }
+        else
+        {
+            // k = i-L so k corresponds to number which
+            // matches in [L,R] interval.
+            k = i-L;
+ 
+            // if Z[k] is less than remaining interval
+            // then Z[i] will be equal to Z[k].
+            // For example, str = "ababab", i = 3, R = 5
+            // and L = 2
+            if (Z[k] < R-i+1)
+                Z[i] = Z[k];
+ 
+            // For example str = "aaaaaa" and i = 2, R is 5,
+            // L is 0
+            else
+            {
+                // else start from R and check manually
+                L = i;
+                while (R<n && str[R-L] == str[R])
+                    R++;
+                Z[i] = R-L;
+                R--;
+            }
+        }
+    }
+}	
+	int isRepeat(string s)
+	{
+	    // Your code goes here
+	    vector<int> z (s.size(),0);
+	    getzarr(s, z);
+	    int n = s.size();
+	    for( int i = 1; i < s.size(); i++)
+	    {
+	        if(z[i] == n - i && z[i]%i == 0)
+	        return 1;
+	        
+	    }
+	    
+	    
+	    
+	    return 0;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+        Solution ob;
+   		cout << ob.isRepeat(str) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
