05210cec928d6046d8d5ff452f97e5090748a577
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-14 22:22:49 +0530

    Time: 144 ms (10.67%), Space: 12.4 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+    int recur(int p1, int p2, string &s1, string &s2, vector<vector<int>> &dp , int &ans)
+    {
+        if(p1 >= s1.size() || p2 >= s2.size())
+            return 0;
+            
+        if(dp[p1][p2] != -1)
+            return dp[p1][p2];
+        
+        dp[p1][p2] = 0;
+        if(s1[p1] == s2[p2])
+          dp[p1][p2] = 1+ recur(p1+1,p2+1,s1,s2,dp,ans);
+        
+         dp[p1][p2] = max(dp[p1][p2] ,recur(p1+1,p2,s1,s2,dp, ans) );
+         dp[p1][p2] = max(dp[p1][p2] ,recur(p1,p2+1,s1,s2,dp, ans) );
+        
+        ans = max(ans, dp[p1][p2]);
+        return dp[p1][p2];
+        
+    }
     int minDistance(string word1, string word2) {
         
-        int dp[word1.size()+1][word2.size()+1];
-        memset(dp, 0, sizeof(dp));
+       vector<vector<int>> dp(word1.size()+1, vector<int> (word2.size() + 1, -1) );
+         
         int ans = 0;
-        int tot = word1.size() + word2.size();
         
-        for( int i = 0; i<word1.size(); i++)
-        {
-            for( int j = 0; j<word2.size(); j++)
-            {
-                dp[i+1][j+1] = max(dp[i][j], dp[i+1][j+1]);
-                dp[i+1][j] = max(dp[i][j], dp[i+1][j]);
-                dp[i][j+1] = max(dp[i][j], dp[i][j+1]);
-                
-                
-                if(word1[i] == word2[j])
-                {
-                    dp[i+1][j+1]= max(dp[i][j]+1, dp[i+1][j+1]);
-                }
-                
-                ans = max(ans, dp[i+1][j+1]);
-            }
-        }
-        
-        return tot - 2*ans;
-    
+        recur(0, 0, word1, word2, dp, ans);
+        return word1.size() + word2.size() - 2*ans;
     }
 };
