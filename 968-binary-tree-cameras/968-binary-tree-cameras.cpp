0796dda0d972df5971d4f2a8be18d51d4102d2d8
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-17 07:06:22 +0530

    Time: 11 ms (83.13%), Space: 21.3 MB (55.06%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -13,50 +13,46 @@ class Solution {
 public:
        //occ == monitored
     
-    void count(TreeNode* root, int &cam_here, int &no_cam_here, int &occ, int &nocc) 
+    void count(TreeNode* root, int &cam_here, int &occ, int &nocc) 
     {
-        int lc,nlc,locc,lnocc,rc,nrc,rocc,rnocc;
+        int lc,locc,lnocc,rc,rocc,rnocc;
        
         if( root -> left ==NULL && root -> right ==NULL)
         {
             cam_here = 1;
-            no_cam_here = 0;
             nocc = 0;
             occ = 1;
             return;
         }
         else if(root -> left == NULL)
         {
-            count(root->right, rc, nrc, rocc, rnocc);
+            count(root->right, rc, rocc, rnocc);
             cam_here = 1 + min(rocc, rnocc);
-            no_cam_here = rc;
-            occ = min (cam_here, no_cam_here);
+            occ = min (rc, cam_here);
             nocc = min (occ, rocc);
             return;
         }
         else if(root -> right == NULL)
         {
-            count(root->left, lc, nlc, locc, lnocc);
+            count(root->left, lc, locc, lnocc);
             cam_here = 1 + min(locc, lnocc);
-            no_cam_here = lc;
-            occ = min (cam_here, no_cam_here);
+            occ = min (cam_here, lc);
             nocc = min (occ, locc);
-            
             return;
         }
         
-        count(root->left, lc, nlc, locc, lnocc);
-        count(root->right, rc, nrc, rocc, rnocc);
+        count(root->left, lc, locc, lnocc);
+        count(root->right, rc, rocc, rnocc);
         
         cam_here = min(locc, lnocc)+min(rocc, rnocc) + 1;
-        no_cam_here = min(locc + rc, rocc + lc) ;
-        occ = min(cam_here, no_cam_here);
-        nocc = locc + rocc ;
+        occ = min(min(locc + rc, rocc + lc), cam_here) ;
+        nocc = locc + rocc;
+     
       // cout<<root->val << cam_here <<" "<<no_cam_here<<" "<<occ<<" "<<nocc<<endl;
     }
     int minCameraCover(TreeNode* root) {
-        int cam_here, no_cam_here, occ, nocc;
-       count(root, cam_here, no_cam_here, occ, nocc);
+        int cam_here, occ, nocc;
+       count(root, cam_here, occ, nocc);
        return occ;
     }
 };
