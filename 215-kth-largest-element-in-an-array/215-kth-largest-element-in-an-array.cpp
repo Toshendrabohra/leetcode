5cc20aed68d2ce360cb37e1fd9c49185efacea9f
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-22 21:32:13 +0530

    Time: 205 ms (5.02%), Space: 9.9 MB (97.44%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,48 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-     
-        priority_queue<int,vector<int>, greater<int>> p;
-        
-        for( int i = 0; i < nums.size(); i++)
+class Solution
+{
+    public:
+        void quickselect ( int &l, int &r, int &pivot, vector<int> &nums, int &k)
         {
-            p.push(nums[i]);
+            int x = l + 1;
             
-            if(p.size() > k)
-                p.pop();
+            for(int i = x; i <= r; i++)
+            {
+                if(nums[i] <= nums[l])
+                {
+                    swap(nums[i], nums[x++]);
+                }
+            }
+            swap(nums[l], nums[x - 1]);
+            
+            if(x - l  == k)
+            {
+                k = 0;
+                l = x - 1;
+                r = l;
+            }
+            else if( x - l  > k)
+            {
+                r = x - 2;
+            }
+            else
+            {
+                k -= (x - l);
+                l = x ;   
+            }
         }
-        
-        return p.top();
-    }
+    
+        int findKthLargest(vector<int> &nums, int k)
+        {
+            int n = nums.size();
+            int l = 0, r = n - 1;
+            k = n - k + 1;
+            while(l < r)
+            {
+                int pivot = l;
+                quickselect(l, r, pivot, nums, k);
+              //  cout<<l<<" "<<r<<" "<<k<<endl;
+            }
+            
+            return nums[l];
+        }
 };
