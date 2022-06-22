904c0a73ef5efd3e53ac4ba3e999b8c2ce525e3d
Toshendra Bohra <48238286+Toshendrabohra@users.noreply.github.com>
2022-06-22 23:59:39 +0530

    Added solution - LeetHub

---
+++ Count subsequences of type a^i, b^j, c^k - GFG/count-subsequences-of-type-ai-bj-ck.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+//User function template 
+
+class Solution{
+  public:
+    // s : given string
+    // return the expected answer
+    int mod = 1e9 + 7;
+    
+    int pow(int x, int y)
+    {
+        if(x ==  1)
+        return 1;
+        
+        if(y == 1 )
+        return x;
+        
+        int r= 1;
+        if(x%2)    
+        r = x;
+        
+        int z = pow(x, y/2);
+        
+        return (z*z*r)%mod;
+    }
+    
+    int fun(string &s) {
+        //code here
+         const long long int MOD = 1e9 + 7;
+        long long int a = 0, ab = 0, abc = 0;
+        
+        for(char ch: s) {
+            // 1. Append a to a's
+            // 2. Start new string with a.
+            if(ch == 'a') a = (2 * a % MOD + 1) % MOD;
+            
+            // 1. Append b to ab's
+            // 2. Append b directly to a's
+            if(ch == 'b') ab = (2 * ab % MOD + a) % MOD;
+            
+            // 1. Append c to abc's
+            // 2. Append c directly to ab's
+            if(ch == 'c') abc = (2 * abc % MOD + ab) % MOD;
+        }
+        
+        return abc;
+    }
+};
+
+// { Driver Code Starts.
+ 
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.fun(s)<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
